ec2d85c6ad08ad0b833cd04f46960118ef89da04
Chonnakarn Rungjaroenporn <chonnak.rung@gmail.com>
2023-10-06 00:31:58 +0700

    turtle problem1

--- pb1.py
+++ pb1.py
@@ -1,3 +1,21 @@
-from turtle import *
+from turtle import Turtle
 
-forward(100)
+t = Turtle()
+
+
+def draw_sqaure(size):
+    sq_iteration = 0
+    while sq_iteration < 4:
+        t.dot()
+        t.color('red')
+        t.forward(size)
+        t.left(90)
+        sq_iteration = sq_iteration+1
+
+
+size_sq = int(input('Input your desired size of square:'))
+draw_sqaure(size_sq)
+
+
+
+t.screen.mainloop()
